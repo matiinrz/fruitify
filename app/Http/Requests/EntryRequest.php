e6e3d535a2916ce3a0d2c88195d0b10c9b6e7abf
Matin <matinphonej7@gmail.com>
2023-07-19 12:30:44 +0330

    Add validation for entry request

---
+++ app/Http/Requests/EntryRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EntryRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'plate' => ['required'],
+            'type' => ['required', 'json'],
+            'entry_date' => ['required']
+        ];
+    }
+}
