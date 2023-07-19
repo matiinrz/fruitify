0a9ed64483ef7bd71c8782a7681d347d469dff98
tohid1994 <tohid.salime1994.29@gmail.com>
2023-07-19 10:58:16 +0330

    entry page complate

---
+++ nuxt/components/typePicker.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-row>
+        <v-col cols="6" class="pl-0">
+            <v-text-field label="نوع محصول" v-model="proType.type" />
+        </v-col>
+        <v-col cols="4" class="px-1">
+            <v-text-field label=" وزن(kg)" type="number" v-model="proType.weight" />
+        </v-col>
+        <v-col cols="2" class="pr-0">
+            <v-btn size="x-small" height="46" width="46" @click="setType()" :disabled="!proType.type || !proType.weight">
+                <v-icon>mdi-check</v-icon>
+            </v-btn>
+        </v-col>
+    </v-row>
+    <v-card class="mb-2" v-if="type.length > 0">
+        <v-card-text>
+            <v-chip closable @click.clear="cleared(item.type)" v-for="(item, i) in type" :key="i">{{
+                item.type +
+                ',' + item.weight
+            }}</v-chip>
+        </v-card-text>
+    </v-card>
+</template>
+<script setup>
+const proType = ref({ type: "", weight: null })
+const type = ref([])
+let emit = defineEmits(['update:modelValue']);
+
+const { modelValue, errors } = defineProps(['modelValue', 'errors'])
+
+const setType = () => {
+    type.value.push(proType.value)
+    proType.value = { type: "", weight: "" }
+}
+
+watch(type, (newValue) => {
+    emit('update:modelValue', newValue)
+})
+
+const cleared = async (value) => {
+    type.value = type.value.filter(arr => {
+        return arr.type !== value;
+    });
+}
+</script>
--- nuxt/pages/entry/index.vue
+++ nuxt/pages/entry/index.vue
@@ -6,15 +6,14 @@
             </v-card-title>
             <v-divider />
             <v-card-text>
-                <v-text-field label="شماره پلاک" type="Number" />
-                <persian-date-picker label="تاریخ" />
-                <v-text-field label="نوع محصول" />
-                <v-text-field label="وزن" type="number" />
-                <camera-picker v-model="image" />
+                <plate-picker v-model="object.plate" />
+                <persian-date-picker label="تاریخ" v-model="object.entry_date" />
+                <type-picker v-model="object.type" />
+                <camera-picker v-model="object.image" />
             </v-card-text>
             <v-divider />
             <v-card-actions>
-                <v-btn block>تایید</v-btn>
+                <v-btn block @click="setEntry()">تایید</v-btn>
             </v-card-actions>
             <v-card-actions>
                 <v-btn block to="/" variant="outlined">بازگشت</v-btn>
@@ -24,4 +23,28 @@
 </template>
 <script setup>
 const image = ref(null)
+// entry
+const object = ref({
+    plate: "",
+    image: "",
+    weight: "",
+    type: [],
+    entry_date: "",
+
+})
+
+const setEntry = async () => {
+    const { data, error } = await api('api/entry', {
+        method: "POST",
+        key: "entry_post",
+        body: { ...object.value }
+    })
+    if (error?.value) {
+        errors.value = error.value.data.errors
+    } else {
+        toast.success("عملیات با موفقیت انجام شد")
+        router.push('/')
+    }
+
+}
 </script>
