6035dfe7bb303e301db7793139ba19637c54c677
Matin <matinphonej7@gmail.com>
2023-07-19 11:37:49 +0330

    Fix json save

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 Route::post('auth/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::resource('entry', EntryController::class);
     Route::resource('egress', EntryController::class);
+    Route::post('auth/logout', [AuthController::class, 'destroy']);
 });
