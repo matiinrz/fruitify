847454cab8d769ca452a44f53d521626d57525e0
Matin <matinphonej7@gmail.com>
2023-08-13 13:52:52 +0330

    Add Fruit api

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -11,7 +11,7 @@ class ExportController extends Controller
 
     public function index(Request $request)
     {
-        return response()->json($request->input('arguments'));
+        return response()->json(json_encode($request->input('arguments')));
         ini_set('memory_limit', '4096M');
         ini_set('post_max_size', '210M');
         set_time_limit(3600);
