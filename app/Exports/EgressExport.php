0b2903977fed7189139d6cd30b53cbb6d128b37d
Matin <matinphonej7@gmail.com>
2023-08-13 13:50:40 +0330

    Add Fruit api

--- app/Exports/EgressExport.php
+++ app/Exports/EgressExport.php
@@ -3,7 +3,9 @@
 namespace App\Exports;
 
 use App\Models\Egress;
+use Illuminate\Console\View\Components\Alert;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -20,11 +22,17 @@ class EgressExport implements FromCollection, ShouldAutoSize, WithHeadings
 
     public function collection(): Collection
     {
+        Log::alert($this->array->get('fruit_id'));
         $egressCollection = Egress::query()
             ->select('egress.id', 'egress.plate', 'fruit.name as fruit_name', 'egress.weight', 'egress.entry_date',
                 'provinces.name as province_name', 'cities.name as city_name', 'stalls.name as stall_name',
                 'halls.name as hall_name', 'egress.fruit_id', 'egress.province_id', 'egress.city_id', 'egress.stall_id',
                 'egress.hall_id', 'egress.created_at')
+            ->join('fruit', 'egress.fruit_id', '=', 'fruit.id')
+            ->join('provinces', 'egress.province_id', '=', 'provinces.id')
+            ->join('cities', 'egress.city_id', '=', 'cities.id')
+            ->join('halls', 'egress.hall_id', '=', 'halls.id')
+            ->join('stalls', 'egress.stall_id', '=', 'stalls.id')
             ->when($this->array->get('province_id'), function ($query, $param) {
                 $query->where('province_id', $param);
             })->when($this->array->get('city_id'), function ($query, $param) {
@@ -42,11 +50,6 @@ class EgressExport implements FromCollection, ShouldAutoSize, WithHeadings
             ->when($this->array->get('created_at_to'), function ($query, $created_at_to) {
                 $query->where('created_at', '>=', "$created_at_to 23:59:59");
             })
-            ->join('fruit', 'egress.fruit_id', '=', 'fruit.id')
-            ->join('provinces', 'egress.province_id', '=', 'provinces.id')
-            ->join('cities', 'egress.city_id', '=', 'cities.id')
-            ->join('halls', 'egress.hall_id', '=', 'halls.id')
-            ->join('stalls', 'egress.stall_id', '=', 'stalls.id')
             ->orderByDesc('created_at')->get();
 
         $egressCollection->map(function ($array) {
