a5849b6bee8bc71496ac81e64e030816e8c67829
Matin <matinphonej7@gmail.com>
2024-01-22 18:22:02 +0330

    Fix Error

--- nuxt/pages/admin/report/index.vue
+++ nuxt/pages/admin/report/index.vue
@@ -2,16 +2,16 @@
     <v-container class="d-flex justify-center">
         <v-card width="600">
             <v-card-title class=" text-center">گزارش گیری</v-card-title>
-            <v-divider />
+            <v-divider/>
             <v-card-subtitle class="d-flex mt-2">
                 <v-select label="نوع گزارش" v-model="reportType" :items="reportsItem" item-title="name"
-                    item-value="value" />
+                          item-value="value"/>
 
                 <v-btn class="mr-2" color="blue" icon="mdi-filter" @click="filtersDialog = true"/>
                 <v-btn class="mr-2" color="green" icon="mdi-download" @click="downloadeExel()"/>
                 <v-btn class="mr-2" color="red darken-4" icon="mdi-file-pdf-box" @click="downloadPdf()"/>
             </v-card-subtitle>
-            <v-divider />
+            <v-divider/>
             <!-- <v-card-actions>
                 <v-btn class="mx-2" block  >خروجی اکسل</v-btn>
 
@@ -19,67 +19,67 @@
             <v-card-text>
                 <v-table>
                     <thead>
-                        <tr>
-                            <th class="text-center text-primary">ردیف</th>
-                            <th class="text-center text-primary"> تصویر پلاک</th>
-                            <th class="text-center text-primary">پلاک</th>
-                            <th class="text-center text-primary">تاریخ</th>
-                            <th class="text-center text-primary">میوه</th>
-                            <th class="text-center text-primary">وزن</th>
-                            <th class="text-center text-primary">مبدا</th>
-                            <th class="text-center text-primary">مقصد</th>
-                            <th class="text-center text-primary"> تصویر بارنامه</th>
-                            <th class="text-center text-primary">عملیات</th>
-                        </tr>
+                    <tr>
+                        <th class="text-center text-primary">ردیف</th>
+                        <th class="text-center text-primary"> تصویر پلاک</th>
+                        <th class="text-center text-primary">پلاک</th>
+                        <th class="text-center text-primary">تاریخ</th>
+                        <th class="text-center text-primary">میوه</th>
+                        <th class="text-center text-primary">وزن</th>
+                        <th class="text-center text-primary">مبدا</th>
+                        <th class="text-center text-primary">مقصد</th>
+                        <th class="text-center text-primary"> تصویر بارنامه</th>
+                        <th class="text-center text-primary">عملیات</th>
+                    </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="(item, i) in reports.data" :key="reports">
-                            <td class="text-center text-primary">{{ i + 1 }}</td>
-                            <td class="text-center">
-                                <v-avatar v-if="item.plate_image">
-                                    <v-img :src="imageUrl(item.plate_image)"
-                                        @click="showImageDialog(imageUrl(item.plate_image))" />
-                                </v-avatar>
-                                <span v-else>-</span>
-                            </td>
-                            <td class="text-center">{{ item.plate }}</td>
-                            <td class="text-center">{{ datetime(item.entry_date) }}</td>
-                            <td class="text-center">{{ item?.fruit?.name }}</td>
-                            <td class="text-center">{{ item?.weight }}</td>
-                            <td class="text-center" v-if="reportType === 'entry'">{{ item?.province?.name }} - {{
+                    <tr v-for="(item, i) in reports.data" :key="reports">
+                        <td class="text-center text-primary">{{ i + 1 }}</td>
+                        <td class="text-center">
+                            <v-avatar v-if="item.plate_image">
+                                <v-img :src="imageUrl(item.plate_image)"
+                                       @click="showImageDialog(imageUrl(item.plate_image))"/>
+                            </v-avatar>
+                            <span v-else>-</span>
+                        </td>
+                        <td class="text-center">{{ item.plate }}</td>
+                        <td class="text-center">{{ datetime(item.entry_date) }}</td>
+                        <td class="text-center">{{ item?.fruit?.name }}</td>
+                        <td class="text-center">{{ item?.weight }}</td>
+                        <td class="text-center" v-if="reportType === 'entry'">{{ item?.province?.name }} - {{
                                 item?.city?.name
                             }}
-                            </td>
-                            <td class="text-center" v-if="reportType === 'entry'">{{ item?.hall?.name }} - {{
+                        </td>
+                        <td class="text-center" v-if="reportType === 'entry'">{{ item?.hall?.name }} - {{
                                 item?.stall?.name
                             }}
-                            </td>
-                            <td class="text-center" v-if="reportType === 'egress'">{{ item?.hall?.name }} - {{
+                        </td>
+                        <td class="text-center" v-if="reportType === 'egress'">{{ item?.hall?.name }} - {{
                                 item?.stall?.name
                             }}
-                            </td>
-                            <td class="text-center" v-if="reportType === 'egress'">{{ item?.province?.name }} - {{
+                        </td>
+                        <td class="text-center" v-if="reportType === 'egress'">{{ item?.province?.name }} - {{
                                 item?.city?.name
                             }}
-                            </td>
-                            <td class="text-center">
-                                <v-avatar v-if="item.image">
-                                    <v-img :src="imageUrl(item.image)" @click="showImageDialog(imageUrl(item.image))" />
-                                </v-avatar>
-                                <span v-else>-</span>
-                            </td>
-                            <td>
-                                <v-btn icon="mdi-pencil" size="x-small" class="text-white"
-                                    :to="`/admin/report/${item.id}/?type=${reportType}`" />
-                            </td>
+                        </td>
+                        <td class="text-center">
+                            <v-avatar v-if="item.image">
+                                <v-img :src="imageUrl(item.image)" @click="showImageDialog(imageUrl(item.image))"/>
+                            </v-avatar>
+                            <span v-else>-</span>
+                        </td>
+                        <td>
+                            <v-btn icon="mdi-pencil" size="x-small" class="text-white"
+                                   :to="`/admin/report/${item.id}/?type=${reportType}`"/>
+                        </td>
 
-                        </tr>
+                    </tr>
                     </tbody>
                 </v-table>
             </v-card-text>
             <v-card-actions class="justify-center">
                 <v-pagination v-model="filters.page" :length="reports.last_page" :total-visible="7"
-                    @update:modelValue="changePage()"></v-pagination>
+                              @update:modelValue="changePage()"></v-pagination>
             </v-card-actions>
         </v-card>
         <v-dialog v-model="filtersDialog" width="600">
@@ -87,25 +87,25 @@
                 <v-card-text>
                     <v-row>
                         <v-col cols="12">
-                            <persian-date-picker label="از تاریخ" v-model="filters.created_at_from" />
+                            <persian-date-picker label="از تاریخ" v-model="filters.created_at_from"/>
                         </v-col>
                         <v-col cols="12">
-                            <persian-date-picker label="تا تاریخ" v-model="filters.created_at_to" />
+                            <persian-date-picker label="تا تاریخ" v-model="filters.created_at_to"/>
                         </v-col>
                         <v-col cols="6">
-                            <province-picker v-model="filters.province_id" />
+                            <province-picker v-model="filters.province_id"/>
                         </v-col>
                         <v-col cols="6">
-                            <city-picker v-model="filters.city_id" :provinceId="filters.province_id" />
+                            <city-picker v-model="filters.city_id" :provinceId="filters.province_id"/>
                         </v-col>
                         <v-col cols="6">
-                            <hall-picker v-model="filters.hall_id" />
+                            <hall-picker v-model="filters.hall_id"/>
                         </v-col>
                         <v-col cols="6">
-                            <stall-picker v-model="filters.stall_id" />
+                            <stall-picker v-model="filters.stall_id"/>
                         </v-col>
                         <v-col cols="6">
-                            <fruit-picker v-model="filters.fruit_id" />
+                            <fruit-picker v-model="filters.fruit_id"/>
                         </v-col>
 
                     </v-row>
@@ -122,7 +122,7 @@ definePageMeta({
     layout: "admin",
     middleware: "auth"
 });
-const { $event } = useNuxtApp()
+const {$event} = useNuxtApp()
 $event('tilte-chaneg', 'لیست ورودی و خروجی')
 
 const config = useRuntimeConfig();
@@ -140,17 +140,17 @@ const filters = ref({
     page: 1
 })
 const reportsItem = ref([
-    { name: 'ورودی', value: 'entry' },
-    { name: 'خروجی', value: 'egress' },
+    {name: 'ورودی', value: 'entry'},
+    {name: 'خروجی', value: 'egress'},
 ])
 
 const reportType = ref("entry")
 
 const getEntry = async () => {
-    const { data, error } = await api('api/entry', {
+    const {data, error} = await api('api/entry', {
         method: "GET",
         key: "get_entry",
-        query: { ...filters.value }
+        query: {...filters.value}
     })
     if (data?.value) {
         reports.value = data.value
@@ -160,10 +160,10 @@ const getEntry = async () => {
 getEntry()
 
 const getEgress = async () => {
-    const { data, error } = await api('api/egress', {
+    const {data, error} = await api('api/egress', {
         method: "GET",
         key: "get_egress",
-        query: { ...filters.value }
+        query: {...filters.value}
     })
     if (data?.value) {
         reports.value = data.value
@@ -185,12 +185,12 @@ const showImageDialog = (image) => {
 }
 
 const downloadeExel = async () => {
-    const { data, error } = await api('api/exports', {
+    const {data, error} = await api('api/exports', {
         method: "POST",
         key: "get_export",
         body: {
             "export": `App\\Exports\\${(reportType.value === 'entry') ? 'Entry' : 'Egress'}Export`,
-            arguments: { ...filters.value }
+            arguments: {...filters.value}
         },
 
 
