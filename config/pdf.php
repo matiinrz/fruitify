cba6168d0a704919af53ff7d08cdb614eb52583c
Matin <matinphonej7@gmail.com>
2024-03-04 18:22:08 +0330

    Fix Error

--- config/pdf.php
+++ config/pdf.php
@@ -8,7 +8,7 @@ return [
 	'keywords'              => '',
 	'creator'               => 'Laravel Pdf',
 	'display_mode'          => 'fullpage',
-	'tempDir'               => base_path('../temp/'),
+	'tempDir'               => storage_path('temp/'),
 	'pdf_a'                 => false,
 	'pdf_a_auto'            => false,
 	'icc_profile_path'      => '',
