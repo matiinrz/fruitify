0e4e7d344c7a572898df02fdc13d6f662482d5f1
volcano <aliwpna@gmail.com>
2024-06-09 18:48:02 +0330

    change icon home

--- nuxt/components/navBtn.vue
+++ nuxt/components/navBtn.vue
@@ -1,7 +1,7 @@
 <template>
     <v-bottom-navigation height="85" :active="active" back>
         <v-btn to="/">
-            <v-img src="/images/home-icon-2.png" width="30" class="mb-2"></v-img>
+            <v-img src="/images/home-icon-3.png" width="30" class="mb-2"></v-img>
 
             خانه
         </v-btn>
